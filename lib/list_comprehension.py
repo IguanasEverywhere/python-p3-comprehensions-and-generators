badc9e526ae9abf62fa28d152508929d88e0a433
Scott Schwab <scottschwab86@gmail.com>
2023-07-23 17:21:21 -0500

    Complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [n for n in num_list if n % 2 == 0]
+    return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamated_sentences = [sentence + '!' for sentence in sentence_list]
+    return exclamated_sentences
